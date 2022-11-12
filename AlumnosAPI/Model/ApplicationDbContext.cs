4ba884dacbf31ce0bf530d55414f9f3c8f83dde9
Octavio Garcia <jss670@gmail.com>
2022-11-11 22:44:29 -0700

    Agregando Migracion Materia

--- AlumnosAPI/Model/ApplicationDbContext.cs
+++ AlumnosAPI/Model/ApplicationDbContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AlumnosAPI.Model.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlumnosAPI.Model
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Alumno> Alumnos { get; set; }
     }
 }
