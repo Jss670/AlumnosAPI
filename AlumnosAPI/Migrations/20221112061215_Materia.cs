1522813023ac7955b155d8f3692c27a4513edb27
Octavio Garcia <jss670@gmail.com>
2022-11-11 23:18:18 -0700

    entidad materia y crea la migracion de la entidad materia

---
+++ AlumnosAPI/Migrations/20221112061215_Materia.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlumnosAPI.Migrations
+{
+    public partial class Materia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Materia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AlumnoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Materia_Alumnos_AlumnoId",
+                        column: x => x.AlumnoId,
+                        principalTable: "Alumnos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materia_AlumnoId",
+                table: "Materia",
+                column: "AlumnoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Materia");
+        }
+    }
+}
---
+++ AlumnosAPI/Model/Entities/Materia.cs
@@ -0,0 +1,9 @@
+﻿namespace AlumnosAPI.Model.Entities
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
